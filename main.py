36bcc27c68a923e0ab0f814d4dfeceefd154c75f
Amilcar <a1000carzh@gmail.com>
2022-10-28 19:40:27 -0500

    Added details

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ files = extract_data("05/2022")
 
 frame = data_frame_process(files)
 
-frame['cod_postal'] = frame['cod_postal'].replace(np.nan,0, regex=True)
+# This fix some empty field is the .csv files
+frame['cod_postal'] = frame['cod_postal'].replace(np.nan, 0, regex=True)
+# This convert double to int in postal code field
 frame['cod_postal'] = frame['cod_postal'].astype(int)
 
 frame = frame.replace(np.nan, '', regex=True)
@@ -21,8 +23,7 @@ fr1 = frame[["nom_estab", "raz_social", "nombre_act", "per_ocu", 'tipo_vial', 'n
 # This take only 1000000 of rows
 ten = fr1.head(1000000)
 
-
-#ten.to_csv('file_name.csv', index=False)
+# ten.to_csv('file_name.csv', index=False)
 # print(ten)
 
 # You can configure your custom database
@@ -32,18 +33,22 @@ database = 'hackaton'
 username = 'admin'
 password = 'a2d0m2i2n'
 
+# Getting ready a database
 connection = pymysql.connect(host=server, database=database, user=username, password=password)
 
 cursor = connection.cursor()
 
 # This insert data to a database
-for i, row in fr1.iterrows():
-    data = (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11],)
-    print(data)
-    # configure your table's name
-    sql = "INSERT INTO hackaton.inegi(var_nombre,var_raz_social,var_actividad,var_total_empl,var_tipo_vialidad,var_vialidad,var_tipo_asent,var_asent,var_cp,var_estado,var_municipio,var_localidad) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-    cursor.execute(sql, data)
-    connection.commit()
+
+# ------This is a sample-----------
+
+# for i, row in fr1.iterrows():
+#    data = (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11],)
+#    print(data)
+#    # configure your table's name
+#    sql = "INSERT INTO hackaton.inegi(var_nombre,var_raz_social,var_actividad,var_total_empl,var_tipo_vialidad,var_vialidad,var_tipo_asent,var_asent,var_cp,var_estado,var_municipio,var_localidad) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+#    cursor.execute(sql, data)
+#    connection.commit()
 
 cursor.close()
 # print(urls)
